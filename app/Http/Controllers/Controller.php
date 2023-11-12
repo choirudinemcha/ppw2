73e6f804a778968fb0b62a7d90c7b8425b359a27
Choirudin Emcha <choirudin.emcha@gmail.com>
2023-11-13 03:32:23 +0700

    api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,22 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     description="Contoh API doc menggunakan OpenAPI/Swagger",
+ *     version="0.0.1",
+ *     title="Contoh API documentation",
+ *     termsOfService="http://swagger.io/terms/",
+ *     @OA\Contact(
+ *         email="choirudin.emchagmail.com"
+ *     ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\InfoController;
+use App\Http\Controllers\GreetController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/info', [InfoController::class, 'index'])->name('info');
+Route::get('/greet', [GreetController::class, 'greet'])->name('greet');
